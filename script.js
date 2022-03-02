a7164a76d2b9eb5c7a1f29e59d0db44c4cb8de92
wiusxt <ashatovanas65@gmail.com>
2022-03-02 14:47:22 +0600

    start working in js file

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.querySelectorAll('.accordion').forEach(accordion => {
-const header = accordion.querySelector('.accordion__header')
+const header = accordion.querySelector('.accordion__header');
+
+header.addEventListener('click', function(){
+    accordion.classList.toggle('accordion--open');
+    
+})
 });
